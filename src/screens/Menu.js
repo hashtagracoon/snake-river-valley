e86d039f66afc8dd220d7831d0fc224ab8d60252
dustin_chiu <hashtagracoon@gmail.com>
2018-10-21 17:28:58 +0800

    add flexbox

--- src/screens/Menu.js
+++ src/screens/Menu.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
+import { View } from 'react-native';
 import { Container, Header, Left, Right, Title, Icon, Content, Card, CardItem, Body, Text, Button } from 'native-base';
 
 export default class Menu extends Component {
+  render() {
+    return (
+      <Container>
 
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name="menu" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Daily Vocabulary</Title>
+          </Body>
+        </Header>
+
+          <View style={{ flex: 1, backgroundColor: 'green', alignItems: 'center', justifyContent: 'center' }}>
+            <Text>ok</Text>
+          </View>
+          <View style={{ flex: 2, backgroundColor: 'yellow', alignItems: 'center', justifyContent: 'center' }}>
+            <Text>hi</Text>
+          </View>
+
+      </Container>
+    );
+  }
+/*
   render() {
     return (
       <Container>
@@ -61,4 +87,5 @@ export default class Menu extends Component {
       </Container>
     );
   }
+  */
 }
