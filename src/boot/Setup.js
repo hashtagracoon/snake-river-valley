31b089a32754b63e851873080951f5f99a7b16dd
dustin_chiu <hashtagracoon@gmail.com>
2018-11-11 12:49:27 +0800

    remove unused code

--- src/boot/Setup.js
+++ src/boot/Setup.js
@@ -13,14 +13,11 @@ import { createStackNavigator } from 'react-navigation';
 import { fromLeft } from 'react-navigation-transitions';
 import getSlideFromRightTransition from 'react-navigation-slide-from-right-transition';
 import { connect } from 'react-redux';
-import { setDbInstance, setWordCache } from "../redux/Actions";
+import { setDbInstance } from "../redux/Actions";
 import SQLite from 'react-native-sqlite-storage';
 SQLite.enablePromise(true);
 import DatabaseSearcher from '../api/DatabaseSearcher';
 import to from '../api/To';
-import WordIndexer from '../asyncstorage/WordIndex';
-import Constants from '../asyncstorage/Constants';
-import { ielts } from '../resources/ielts';
 import { logger } from '../api/Debugger';
 
 let getCustomPushNotification = (handleNotification) => {
@@ -41,7 +38,6 @@ const CustomTransitionConfig = () => {
       const { route } = scene;
       const params = route.params || {};
       const transition = params.transition || null;
-      //console.log(scene);
 
       if(transition === 'fromLeft') {
         return fromLeft();
@@ -76,25 +72,11 @@ const AppNavigator = createStackNavigator(
 
 class Setup extends Component {
 
-  errorCB(err) {
-    console.log("SQL Error: " + err);
-  }
-
-  successCB() {
-    console.log("SQL executed fine");
-  }
-
-  openCB() {
-    console.log("Database OPENED");
-    this.props.setDbInstance()
-  }
-
   openDatabase = async () => {
     let [err, db] = await to(SQLite.openDatabase({name: 'sqlite-31-full-complete.db', createFromLocation : "~/sqlite-31-full-complete.db", location: 'Library'}));
     if(!err) {
       console.log('Database Opened!');
       this.props.setDbInstance(db);
-      this.cacheWords(db);
       SplashScreen.hide();
       console.log('===================== splash screen closed ========================');
     }
@@ -104,55 +86,9 @@ class Setup extends Component {
     }
   }
 
-  cacheWords = async (db) => {
-    const mostCommonIndex = await WordIndexer.getWordIndex('mostCommon');
-    const ieltsIndex = await WordIndexer.getWordIndex('ielts');
-    const toeflIndex = await WordIndexer.getWordIndex('toefl');
-    const greIndex = await WordIndexer.getWordIndex('gre');
-    const satIndex = await WordIndexer.getWordIndex('sat');
-
-    console.log("ieltsIndex = ");
-    console.log(Number(ieltsIndex));
-    console.log("cahce helf length = " + Constants.wordCacheHalfLength);
-
-    if(ieltsIndex <= Constants.wordCacheHalfLength) {
-      for(let i = 0; i < Constants.wordCacheLength; i++) {
-        let [err, data] = await to(DatabaseSearcher.searchDatabase(ielts[i], db));
-        this.props.setWordCache({
-          type: 'ielts',
-          operation: 'PUSH',
-          content: data
-        });
-      }
-    }
-    else if(ieltsIndex + Constants.wordCacheHalfLength >= Constants.ieltsLength) {
-      for(let i = Constants.ieltsLength - 1; i >= Constants.ieltsLength - Constants.wordCacheLength; i--) {
-        let [err, data] = await to(DatabaseSearcher.searchDatabase(ielts[i], db));
-        this.props.setWordCache({
-          type: 'ielts',
-          operation: 'UNSHIFT',
-          content: data
-        });
-      }
-    }
-    else {
-      for(let i = ieltsIndex - Constants.wordCacheHalfLength; i < ieltsIndex + Constants.wordCacheLength; i++) {
-        let [err, data] = await to(DatabaseSearcher.searchDatabase(ielts[i], db));
-        this.props.setWordCache({
-          type: 'ielts',
-          operation: 'PUSH',
-          content: data
-        });
-      }
-    }
-  }
-
   async componentDidMount() {
 
     await this.openDatabase();
-
-    // do stuff while splash screen is shown
-    // After having done stuff (such as async tasks) hide the splash screen
 
     let customPushNotification = getCustomPushNotification(this.handleNotification);
 
@@ -187,7 +123,6 @@ class Setup extends Component {
 export default connect(
   null,
   {
-    setDbInstance: setDbInstance,
-    setWordCache: setWordCache
+    setDbInstance: setDbInstance
   }
 )(Setup);
